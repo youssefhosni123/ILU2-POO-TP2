b1a903265a01a914a6f5440830fc5203ec11368d
youssefhosni123 <hosniyoussef36@gmail.com>
2024-03-29 14:59:48 +0100

    testControle1cb

---
+++ test/controleur/ControlAcheterProduitTest.java
@@ -0,0 +1,36 @@
+package controleur;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ControlAcheterProduitTest {
+
+	@Test
+	void testControlAcheterProduit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testVerifierIdentite() {
+		//fail("Not yet implemented");
+		ControlAcheterProduit controlAcheterProduit = new ControlAcheterProduit();
+		boolean result = 
+	}
+
+	@Test
+	void testTrouverVendeurProduit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTrouverEtalVendeur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAcheterProduit() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/controleur/ControlEmmenagerTest.java
@@ -0,0 +1,54 @@
+package controleur;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import personnages.Chef;
+import villagegaulois.Village;
+
+class ControlEmmenagerTest {
+	private Village village;
+	private Chef abraracourcix;
+	@BeforeEach
+	public void initialiserSituation() {
+		System.out.println("Initialisation...");
+		village = new Village("le village des irréductibles", 10, 5);
+		abraracourcix = new Chef("Abraracourcix", 10, village);
+		village.setChef(abraracourcix);
+	}
+
+	@Test
+	void testControlEmmenager() {
+		ControlEmmenager controlEmmenager = new ControlEmmenager(village);
+		assertNotNull(controlEmmenager, "Constructeur ne renvoie pas null");
+
+	}
+
+	@Test
+	void testIsHabitant() {
+		ControlEmmenager controlEmmenager = new ControlEmmenager(village);
+		controlEmmenager.ajouterGaulois("Bonemine", 10);
+		assertTrue(controlEmmenager.isHabitant("Bonemine"));
+		assertFalse(controlEmmenager.isHabitant("Existe pas"));
+		controlEmmenager.ajouterDruide("Panoramix", 10, 1, 5);
+		assertTrue(controlEmmenager.isHabitant("Panoramix"));
+	}
+		
+	
+
+	@Test
+	void testAjouterDruide() {
+		ControlEmmenager controlEmmenager = new ControlEmmenager(village);
+		controlEmmenager.ajouterDruide("Panoramix", 10, 1, 5);
+		// pas de vérification
+	}
+
+	@Test
+	void testAjouterGaulois() {
+		ControlEmmenager controlEmmenager = new ControlEmmenager(village);
+		controlEmmenager.ajouterGaulois("Bonemine", 10);
+		// pas de vérification
+	}
+
+}
